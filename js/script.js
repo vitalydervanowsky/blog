bcfe4b26a9a8ece9142ec19efac260cb6e489bb0
vitalydervanowsky <vitalydervanowsky@gmail.com>
2016-06-01 21:06:48 +0300

    6

--- js/script.js
+++ js/script.js
@@ -1,25 +1,32 @@
 var supportPhone = document.querySelector(".support-phone");
 var supportPhoneL = supportPhone.querySelector(".support-phone-visible");
-var supportPhoneP = supportPhone.querySelector(".support-phone-free");
+var supportPhoneP = supportPhone.querySelector(".support-phone-visually-hidden");
 var supportPhoneC = supportPhone.querySelector(".support-phone-close");
+// var overlay = document.querySelector(".overlay");
 
 window.addEventListener("keydown", function(event) {
 	if (event.keyCode === 27) {
-		if (supportPhoneP.classList.contains("support-phone-free-show")) {
-			supportPhoneP.classList.remove("support-phone-free-show");
-			supportPhoneC.classList.remove("support-phone-close-show");
+		if (supportPhoneP.classList.contains("support-phone-visually-hidden-show")) {
+			supportPhoneP.classList.remove("support-phone-visually-hidden-show");
+			// overlay.classList.remove("overlay-show");
 		}
 	}
 });
 
 supportPhoneL.addEventListener("click", function(event) {
 	event.preventDefault();
-	supportPhoneP.classList.add("support-phone-free-show");
-	supportPhoneC.classList.add("support-phone-close-show");
+	supportPhoneP.classList.add("support-phone-visually-hidden-show");
+	// overlay.classList.add("overlay-show");
 });
 
 supportPhoneC.addEventListener("click", function(event) {
 	event.preventDefault();
-	supportPhoneP.classList.remove("support-phone-free-show");
-	supportPhoneC.classList.remove("support-phone-close-show");
+	supportPhoneP.classList.remove("support-phone-visually-hidden-show");
+	// overlay.classList.remove("overlay-show");
 });
+
+// overlay.addEventListener("click", function(event) {
+// 	event.preventDefault();
+// 	overlay.classList.remove("overlay-show");
+// 	supportPhoneP.classList.remove("support-phone-visually-hidden-show");
+// });
